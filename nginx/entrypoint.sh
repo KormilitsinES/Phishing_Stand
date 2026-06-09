e674d243664a544e826d4f26d6aa3548c2c27f69
KormilitsinES <es.kormilitsin@yandex.ru>
2026-06-09 12:10:44 +0700

    Postfix шалит

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo "[*] Шаблонизация nginx.conf с использованием переменных окружения..."
-envsubst '${TRACK_DOMAIN} ${EVIL_DOMAIN} ${TLS_CERT_FILE} ${TLS_KEY_FILE}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-echo "[+] nginx.conf успешно сгенерирован для доменов ${TRACK_DOMAIN} и ${EVIL_DOMAIN}."
+envsubst '${TRACK_DOMAIN} ${EVIL_DOMAIN} ${BASE_DOMAIN}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+echo "[+] nginx.conf успешно сгенерирован для доменов ${TRACK_DOMAIN}, ${EVIL_DOMAIN} и ${BASE_DOMAIN}."
 
 echo "[*] Шаблонизация mta-sts.txt с использованием переменных окружения..."
-envsubst '${MX_DOMAIN}' < /var/www/html/mta-sts.txt.template > /var/www/html/mts-sts.txt
+envsubst '${MX_DOMAIN}' < /var/www/html/mta-sts.txt.template > /var/www/html/mta-sts.txt
 echo "[+] mta-sts.txt успешно сгенерирован для домена ${MX_DOMAIN}."
 
 exec "$@"
--- postfix/entrypoint.sh
+++ postfix/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "[*] Шаблонизация main.cf с использованием переменных окружения..."
-envsubstr '${HOSTNAME} ${ALLOWED_SENDER_DOMAINS} ${SMTPD_TLS_CERT_FILE} ${SMTPD_TLS_KEY_FILE}' < /etc/postfix/main.cf.template > /etc/postfix/main.cf
+envsubst '${HOSTNAME} ${ALLOWED_SENDER_DOMAINS}' < /etc/postfix/main.cf.template > /etc/postfix/main.cf
 echo "[+] main.cf успешно сгенерирован для домена ${ALLOWED_SENDER_DOMAINS}."
 
-exec "$@"
+exec /scripts/run.sh
