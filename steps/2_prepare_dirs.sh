7fe73be283cf8052e848fd3c51d738364dbf2390
KormilitsinES <es.kormilitsin@yandex.ru>
2026-06-11 09:57:05 +0700

    Рабочая версия (проверена на Ubuntu 22.04)

--- steps/2_prepare_dirs.sh
+++ steps/2_prepare_dirs.sh
@@ -6,6 +6,6 @@ mkdir -p postfix/dkim evilginx2/phishlets evilginx2/data certs
 
 touch gophish/gophish.db
 
-chmod 744 gophish/gophish.db
+chmod 666 gophish/gophish.db
 
 echo -e "${GREEN}[+] Директории созданы.${NC}"
